af5172a4ad2a93c95d4d057d9cb4b3ce2102d845
Chepicov Mark <chepicov.mark@mail.ru>
2019-09-17 23:39:53 +0300

    add pagees

--- scripts/index.js
+++ scripts/index.js
@@ -19,3 +19,15 @@ const openForm = (id) => {
 const toggleDesc = (group) => (id) => {
   document.getElementsByClassName(`${group} desc`)[id - 1].classList.toggle('desc--shown');
 }
+
+let prevScrollpos = window.pageYOffset;
+window.addEventListener('scroll', () => {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('header').classList.add('header--shown');
+  } else if (prevScrollpos + 30 < currentScrollPos) {
+    document.getElementById('header').classList.remove('header--shown');
+    document.getElementById('slideDown').classList.remove('header__menu--shown');
+  }
+  prevScrollpos = currentScrollPos;
+});
--- scripts/slick.js
+++ scripts/slick.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
-  $('.carousel').slick({
+  $('.carousel.carousel--tv').slick({
     dots: true,
     infinite: false,
     arrows: false,
     centerPadding: '60px',
     adaptiveHeight: true,
     slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 5000,
     responsive: [
       {
         breakpoint: 768,
         settings: {
-          dots: true,
-          infinite: false,
-          arrows: false,
           centerPadding: '40px',
           slidesToShow: 3,
         }
@@ -20,12 +19,62 @@ $(document).ready(function(){
       {
         breakpoint: 480,
         settings: {
-          dots: true,
-          arrows: false,
           centerMode: true,
           centerPadding: '20px',
-          infinite: false,
-          slidesToShow: 1
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+  $('.carousel.carousel--tech').slick({
+    dots: true,
+    infinite: false,
+    arrows: false,
+    centerPadding: '60px',
+    adaptiveHeight: true,
+    slidesToShow: 3,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          centerPadding: '40px',
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: true,
+          centerPadding: '20px',
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+  $('.carousel.carousel--review').slick({
+    dots: true,
+    infinite: false,
+    arrows: false,
+    autoplay: true,
+    centerPadding: '60px',
+    adaptiveHeight: true,
+    slidesToShow: 3,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          centerPadding: '40px',
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: true,
+          centerPadding: '20px',
+          slidesToShow: 1,
         }
       }
     ]
