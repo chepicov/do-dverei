507b1b5df0dc364492cdaff8652dc0872e7f60f1
Chepicov Mark <chepicov.mark@mail.ru>
2019-09-24 19:26:08 +0300

    update motoblok

--- motoblock/scripts/slick.js
+++ motoblock/scripts/slick.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.carousel.carousel--tv').slick({
+  $('.carousel.carousel--moto').slick({
     dots: true,
     infinite: true,
     arrows: false,
@@ -10,11 +10,22 @@ $(document).ready(function(){
     autoplaySpeed: 5000,
     responsive: [
       {
+        breakpoint: 2048,
+        settings: 'unslick',
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          centerPadding: '60px',
+          slidesToShow: 3,
+        },
+      },
+      {
         breakpoint: 768,
         settings: {
           centerPadding: '40px',
           slidesToShow: 1,
-        }
+        },
       },
       {
         breakpoint: 480,
@@ -22,35 +33,9 @@ $(document).ready(function(){
           centerMode: true,
           centerPadding: '20px',
           slidesToShow: 1,
-        }
-      }
-    ]
-  });
-  $('.carousel.carousel--tech').slick({
-    dots: true,
-    infinite: true,
-    arrows: false,
-    centerPadding: '60px',
-    adaptiveHeight: true,
-    slidesToShow: 3,
-    autoplay: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          centerPadding: '40px',
-          slidesToShow: 1,
-        }
+        },
       },
-      {
-        breakpoint: 480,
-        settings: {
-          centerMode: true,
-          centerPadding: '20px',
-          slidesToShow: 1,
-        }
-      }
-    ]
+    ],
   });
   $('.carousel.carousel--review').slick({
     dots: true,
